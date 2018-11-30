563c0426c3696ec6d42eb2b28206d98eb8289325
Nguyễn Hữu Thành <hoanganhnh2009@gmail.com>
2018-11-30 19:46:42 +0700

    Update App 30/11/2018 19:46:42

--- index.js
+++ index.js
@@ -226,9 +226,9 @@ login(
                         return this[Math.floor(Math.random() * this.length)];
                     }
                     console.log("FormID: " + message.threadID + '->Message: ' + message.body);
-                    api.sendMessage(listRandomQuestion.rand(), message.threadID)
-                    api.sendMessage("\n \n -------I S2 U------\nTin nhắn trả lời tự động.\n Bạn muốn tìm hiểu thêm thông tin về tôi? HD:  \n- Trả lời: 'fb' để ghé thăm tường của tôi. \n- Trả lời: 'sdt' để lấy số điện thoại của tôi. \n- Trả lời kèm 'stop' ở đầu câu để tránh chatbot tự động trả lời. \n- Trả lời bất kỳ để tiếp tục cuộc trò chuyện. \n" + message.body, message.threadID);
-                    api.markAsRead(message.threadID);
+                    // api.sendMessage(listRandomQuestion.rand(), message.threadID)
+                    // api.sendMessage("\n \n -------I S2 U------\nTin nhắn trả lời tự động.\n Bạn muốn tìm hiểu thêm thông tin về tôi? HD:  \n- Trả lời: 'fb' để ghé thăm tường của tôi. \n- Trả lời: 'sdt' để lấy số điện thoại của tôi. \n- Trả lời kèm 'stop' ở đầu câu để tránh chatbot tự động trả lời. \n- Trả lời bất kỳ để tiếp tục cuộc trò chuyện. \n" + message.body, message.threadID);
+                    // api.markAsRead(message.threadID);
                     return;
                 } else {
                     api.sendMessage('Chào bạn, Đây có phải là số điện thoại của bạn ' + isPhone[0], message.threadID)
